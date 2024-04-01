8712970883a3438633c5951bbc1a3c601be42180
being-devahmad <devahmad41@gmail.com>
2024-04-01 18:57:37 +0500

    api call using createAsyncThunk

--- src/features/ProductSlice.jsx
+++ src/features/ProductSlice.jsx
@@ -1,28 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const productSlice = createSlice({
-    name: "product",
-    initialState: [],
-    reducers: {
-        setProducts(state, action) {
-            return action.payload
-        }
+// createAsyncThunk
+export const fetchDataFromAPI = createAsyncThunk('fetchDataFromAPI', async () => {
+    try {
+        const response = await fetch("https://api.sampleapis.com/coffee/hot");
+        return response.json();
+    } catch (error) {
+        console.log("Error fetching data", error);
     }
 })
 
-export const { setProducts } = productSlice.actions
+
+const productSlice = createSlice({
+    name: "product",
+    initialState: {
+        isLoading: false,
+        isError: false,
+        data: null
+    },
+    extraReducers: (buiilder) => {
+        buiilder.addCase(fetchDataFromAPI.pending, (state, action) => {
+            state.isLoading = true
+        })
+        buiilder.addCase(fetchDataFromAPI.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.data = action.payload
+        })
+        buiilder.addCase(fetchDataFromAPI.rejected, (state, action) => {
+            console.log("Error from API fetch", action.payload)
+            state.isError = true
+        })
+    }
+
+})
+
 export default productSlice.reducer
 
-// Async thunk middleware
-export const fetchDataFromAPI = () => {
-    return async function fetchProductsThunk(dispatch) {
-        try {
-            const response = await fetch("https://api.sampleapis.com/coffee/hot");
-            const data = await response.json();
-            dispatch(setProducts(data)); 
-            console.log(data);
-        } catch (error) {
-            console.log("Error fetching data", error);
-        }
-    };
-};
